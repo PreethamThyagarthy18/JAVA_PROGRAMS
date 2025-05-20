8769924d0d0d1f13b07a806d013dc569a215dcb3
Preetham-Thyagarthy <preethamthyagarthy@gmail.com>
2025-05-21 01:17:27 +0530

    Arrays

--- Ascending.java
+++ Ascending.java
@@ -7,6 +7,6 @@ public class Ascending
         int[] arr = {32, 45, 11, 12, 34, 56, 110};
         Arrays.sort(arr);
         System.out.println("The ascending order is:"+Arrays.toString(arr));
-      
+
     }
 }
